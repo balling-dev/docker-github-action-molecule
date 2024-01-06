0a3abbb995f5d3086fd1754360c10a46eff8b458
Kristoffer Winther Balling <balling_cc@k.wbnet.dk>
2024-01-06 01:13:41 +0100

    fix: unbound bash variable

--- release.sh
+++ release.sh
@@ -2,20 +2,26 @@
 
 set -euf -o pipefail
 
+TAG=${1:-}
+
 if ! command -v git-cliff &>/dev/null; then
-  echo "git-cliff is not installed. Run 'cargo install git-cliff' to install it"
+	echo "git-cliff is not installed. Run 'cargo install git-cliff' to install it"
 fi
 
-if [ -z "$1" ]; then
+if [ -z "${TAG}" ]; then
 	echo "Please provide a tag."
 	echo "Usage: ./release.sh v[X.Y.Z]"
 	exit
 fi
 
-echo "Preparing $1..."
+echo "Preparing ${TAG}..."
 # update the changelog
-git-cliff --config cliff.toml --tag "$1" >CHANGELOG.md
-git add -A && git commit -m "chore(release): prepare for $1"
+git-cliff --config cliff.toml --tag "${TAG}" >CHANGELOG.md
+git add -A &&
+	git -c user.name="Kristoffer Winther Balling" \
+		-c user.email="balling_cc@k.wbnet.dk" \
+		-c user.signingkey="830658948EB172F0!" \
+		commit -m "chore(release): prepare for ${TAG}"
 git show
 # generate a changelog for the tag message
 export GIT_CLIFF_TEMPLATE="\
@@ -30,7 +36,7 @@ changelog=$(git-cliff --config cliff_tag.toml --unreleased --strip all)
 git -c user.name="Kristoffer Winther Balling" \
 	-c user.email="balling_cc@k.wbnet.dk" \
 	-c user.signingkey="830658948EB172F0!" \
-	tag -s -a "$1" -m "Release $1" -m "$changelog"
-git tag -v "$1"
+	tag -s -a "${TAG}" -m "Release ${TAG}" -m "$changelog"
+git tag -v "${TAG}"
 echo "Done!"
 echo "Now push the commit (git push) and the tag (git push --tags)."
